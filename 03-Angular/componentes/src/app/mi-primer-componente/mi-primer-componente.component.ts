8343d4424a3d892d8a4b3f31e5306d0672445904
FernandoPogo <fernandopogo96@live.com>
2019-12-01 23:47:04 -0500

    Actualizacion 25 - Nov

--- 03-Angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ 03-Angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,18 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, OnDestroy, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.scss']
 })
-export class MiPrimerComponenteComponent implements OnInit {
+export class MiPrimerComponenteComponent
+  implements OnInit, OnDestroy {
+  @Input()
+  public titulo:string;
 
-  constructor() { }
+  @Input()
+  public imagen: string;
 
-  ngOnInit() {
+  @Input()
+  public textoImagen: string;
+
+  @Input()
+  public height: string
+
+  @Input()
+  public width: string
+
+  @Input()
+  public textoBoton: string;
+
+  @Output() // EVENTO
+  public cambio_sueldo = new EventEmitter();
+
+  constructor() {
+    console.log('Instanciado');
+  } // instacia la clase (Ayuda -> Angular)
+
+  ngOnInit():void { // La clase esta lista
+    console.log('Esta Listo');
+    console.log(this.titulo);
+    console.log(this.textoImagen);
+    console.log(this.textoBoton);
+  }
+
+  ngOnDestroy(): void{ // La clase se destruye
+    console.log('Se destruye');
   }
 
   saludar(){
     alert("Holaaaaa");
   }
+
+  aumentarSueldo(){
+    this.textoBoton = (Number(this.textoBoton) + 1).toString();
+    this.width = (Number(this.width.substr(0,this.width.length -2))+100).toString()+"px";
+    this.height = (Number(this.height.substr(0,this.height.length-2))+100).toString()+"px";
+    // this.textoBoton = (+ this.textoBoton).toString();
+    // this.cambio_sueldo.emit(this.textoBoton);
+  }
 }
+
+// class (etiqueta html) (web-component)
+// Instanciar a la clase
+// Clase esta lista
+// Clase se destruye
+
